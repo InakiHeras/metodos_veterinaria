4bce789186049dd0e60241aa05eba9662e32fe10
ManuelMartinez4102 <200300578@ucaribe.edu.mx>
2024-11-17 23:55:37 -0500

    historial,con correcciones

--- resources/js/Pages/Historial.jsx
+++ resources/js/Pages/Historial.jsx
@@ -6,6 +6,16 @@ export default function Historial({ historial }) {
     const [searchTerm, setSearchTerm] = useState(""); // Estado para búsqueda
     const [isModalOpen, setIsModalOpen] = useState(false); // Estado para abrir/cerrar el modal
 
+    const [formData, setFormData] = useState({
+        fecha: "",
+        medico: "",
+        cliente: "",
+        mascota: "",
+        razon: "",
+        diagnostico: "",
+        recetas: [{ nombre_medicamento: "" }], // Campo de recetas con un solo campo vacío por defecto
+    });
+
     // Filtrar el historial según el término de búsqueda
     const filteredHistorial = historial.filter((item) => {
         const searchIn = `${item.fecha} ${item.diagnostico} ${item.razon} ${item.veterinario} ${item.cliente} ${item.mascota}`;
@@ -22,9 +32,45 @@ export default function Historial({ historial }) {
         setIsModalOpen(false);
     };
 
-    // Función para guardar (puedes expandirla según necesites)
+    // Manejar el cambio de los campos del formulario
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    // Manejar el cambio en las recetas
+    const handleRecetaChange = (index, e) => {
+        const newRecetas = [...formData.recetas];
+        newRecetas[index].nombre_medicamento = e.target.value;
+        setFormData({
+            ...formData,
+            recetas: newRecetas,
+        });
+    };
+
+    // Agregar una receta más
+    const addReceta = () => {
+        setFormData({
+            ...formData,
+            recetas: [...formData.recetas, { nombre_medicamento: "" }],
+        });
+    };
+
+    // Eliminar una receta
+    const removeReceta = (index) => {
+        const newRecetas = formData.recetas.filter((_, i) => i !== index);
+        setFormData({
+            ...formData,
+            recetas: newRecetas,
+        });
+    };
+
+    // Función para guardar el formulario
     const handleSave = () => {
-        console.log("Formulario guardado");
+        console.log("Formulario guardado", formData);
         closeModal();
     };
 
@@ -42,7 +88,7 @@ export default function Historial({ historial }) {
                             className="w-full max-w-xs px-6 py-3 border-none rounded-full bg-pink-200 text-black placeholder-white focus:outline-none focus:ring-2 focus:ring-pink-400"
                         />
                     </div>
-                    <div className="overflow-x-auto max-h-96 overflow-y-auto border rounded-lg">
+                    <div className="overflow-x-auto max-h-96 overflow-y-auto border rounded-lg mb-20">
                         <table className="w-full text-left border-collapse table-auto">
                             <thead className="bg-pink-200 text-pink-800 sticky top-0 z-10">
                                 <tr>
@@ -110,45 +156,116 @@ export default function Historial({ historial }) {
                         </button>
                     </div>
                 </div>
+            </div>
 
-                {/* Modal */}
-                {isModalOpen && (
-                    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-                        <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm w-full">
-                            <h2 className="text-center text-lg font-bold mb-4">Formulario</h2>
-                            <div className="mb-4">
-                                {/* Aquí puedes agregar los campos del formulario */}
-                                <input
-                                    type="text"
-                                    placeholder="Campo 1"
-                                    className="w-full px-4 py-2 border rounded mb-2"
-                                />
-                                <input
-                                    type="text"
-                                    placeholder="Campo 2"
-                                    className="w-full px-4 py-2 border rounded"
-                                />
-                            </div>
+            {/* Modal */}
+            {isModalOpen && (
+                <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+                    <div
+                        className="bg-cover bg-center p-6 rounded-lg shadow-lg max-w-2xl w-full"
+                        style={{
+                            backgroundImage: "url('/assets/formulario.png')",
+                            backgroundSize: "cover",
+                            backgroundPosition: "center",
+                        }}
+                    >
+                        <h2 className="text-center text-lg font-bold text-white mb-4">Formulario</h2>
+                        <div className="grid grid-cols-2 gap-4 mb-4">
+                            {/* Campos del formulario en dos columnas */}
+                            <input
+                                type="date"
+                                name="fecha"
+                                value={formData.fecha}
+                                onChange={handleChange}
+                                className="w-full px-4 py-2 border rounded"
+                            />
+                            <input
+                                type="text"
+                                name="medico"
+                                placeholder="Médico"
+                                value={formData.medico}
+                                onChange={handleChange}
+                                className="w-full px-4 py-2 border rounded"
+                            />
+                            <input
+                                type="text"
+                                name="cliente"
+                                placeholder="Cliente"
+                                value={formData.cliente}
+                                onChange={handleChange}
+                                className="w-full px-4 py-2 border rounded"
+                            />
+                            <input
+                                type="text"
+                                name="mascota"
+                                placeholder="Mascota"
+                                value={formData.mascota}
+                                onChange={handleChange}
+                                className="w-full px-4 py-2 border rounded"
+                            />
+                            <input
+                                type="text"
+                                name="razon"
+                                placeholder="Razón"
+                                value={formData.razon}
+                                onChange={handleChange}
+                                className="w-full px-4 py-2 border rounded"
+                            />
+                            <input
+                                type="text"
+                                name="diagnostico"
+                                placeholder="Diagnóstico"
+                                value={formData.diagnostico}
+                                onChange={handleChange}
+                                className="w-full px-4 py-2 border rounded"
+                            />
+                        </div>
 
-                            <div className="flex justify-between">
-                                {/* Botones Guardar y Cancelar */}
-                                <button
-                                    onClick={handleSave}
-                                    className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
-                                >
-                                    Guardar
-                                </button>
-                                <button
-                                    onClick={closeModal}
-                                    className="bg-gray-300 text-black px-4 py-2 rounded hover:bg-gray-400"
-                                >
-                                    Cancelar
-                                </button>
-                            </div>
+                        <div className="space-y-4 mb-4">
+                            {formData.recetas.map((receta, index) => (
+                                <div key={index} className="flex items-center">
+                                    <input
+                                        type="text"
+                                        name={`receta-${index}`}
+                                        value={receta.nombre_medicamento}
+                                        onChange={(e) => handleRecetaChange(index, e)}
+                                        placeholder="Nombre de medicamento"
+                                        className="w-full px-4 py-2 border rounded mr-2"
+                                    />
+                                    <button
+                                        onClick={() => removeReceta(index)}
+                                        className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+                                    >
+                                        Eliminar receta
+                                    </button>
+                                </div>
+                            ))}
+                            <button
+                                onClick={addReceta}
+                                className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+                            >
+                                Agregar receta
+                            </button>
+                        </div>
+
+                        {/* Botones Guardar y Cancelar */}
+                        <div className="flex justify-between mt-4">
+                            <button
+                                onClick={handleSave}
+                                className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
+                            >
+                                Guardar
+                            </button>
+                            <button
+                                onClick={closeModal}
+                                className="bg-gray-300 text-black px-4 py-2 rounded hover:bg-gray-400"
+                            >
+                                Cancelar
+                            </button>
                         </div>
                     </div>
-                )}
-            </div>
+                </div>
+            )}
         </Base>
     );
 }
